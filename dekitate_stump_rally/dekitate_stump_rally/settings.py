3319bf1e525315159b068d4c3d51d9ee2e931ecc
WagglyZebra9743 <wagglyzebra9743maze@gmail.com>
2026-04-30 20:59:41 +0900

    update databese

--- dekitate_stump_rally/dekitate_stump_rally/settings.py
+++ dekitate_stump_rally/dekitate_stump_rally/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,6 +98,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
 
 # Password validation
